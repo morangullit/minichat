e45b4ac7cb5eb8df1dfbd5e8f8bb2cb6db326a8a
Gullit Enrique Moran Escobar <gmoran@elmarques.com>
2024-05-22 17:33:53 -0500

    Cambiando URL

--- models/server.js
+++ models/server.js
@@ -7,7 +7,7 @@ const Sockets = require('./sockets');
 class Server {
     constructor() {
         this.app = express();
-        this.port = process.env.PORT || 3000;
+        this.port = process.env.PORT || 8080;
 
         // Crear servidor HTTP
         this.server = http.createServer(this.app);
