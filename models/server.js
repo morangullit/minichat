4a5b36b265dba3c6b6f02032f595092569f011d2
Gullit Enrique Moran Escobar <gmoran@elmarques.com>
2024-05-22 17:25:25 -0500

    Cambiando todo

--- models/server.js
+++ models/server.js
@@ -1,46 +1,47 @@
-// Servidor de Express
-const express   = require('express');
-const http      = require('http');
-const socketio  = require('socket.io');
-const path      = require('path');
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
 const Sockets = require('./sockets');
 
 class Server {
-
-    constructor(){
+    constructor() {
         this.app = express();
-        this.port = process.env.PORT || 8080;
+        this.port = process.env.PORT || 3000;
 
-        // http server
-        this.server = http.createServer( this.app );
-        
-        // configuraciones del socket
-        this.io = socketio( this.server, {/* Configuraciones */} );
+        // Crear servidor HTTP
+        this.server = http.createServer(this.app);
+
+        // Configurar Socket.IO en el servidor
+        this.io = socketio(this.server, {
+            cors: {
+                origin: "*",
+                methods: ["GET", "POST"]
+            }
+        });
+
+        // Inicializar middlewares
         this.middlewares();
+
+        // Configurar sockets
         this.configurarSocket();
     }
 
     middlewares() {
-        this.app.use(express.static( path.resolve(__dirname, '../public')));
+        // Servir archivos estáticos
+        this.app.use(express.static(path.resolve(__dirname, '../public')));
     }
 
-    configurarSocket(){
+    configurarSocket() {
         new Sockets(this.io);
     }
 
-    execute () {
-
-        // Inicializar Middlewares
-        //this.middlewares();
-
-        // Inicializar Server
-        this.server.listen( this.port, () => {
-            console.log('Server corrindo en el puerto:', this.port);
+    execute() {
+        // Iniciar el servidor
+        this.server.listen(this.port, () => {
+            console.log(`Server corriendo en el puerto: ${this.port}`);
         });
     }
-
-
 }
-
 
 module.exports = Server;
--- models/sockets.js
+++ models/sockets.js
@@ -1,40 +1,23 @@
-
-
 class Sockets {
-
-    constructor( io ){
+    constructor(io) {
         this.io = io;
         this.socketEvents();
     }
 
-    socketEvents(){
-        
-    this.io.on('connection', ( socket ) => { 
+    socketEvents() {
+        // Evento de conexión
+        this.io.on('connection', (socket) => {
+            console.log('Nuevo cliente conectado');
 
-        //Este codigo es para saber si esta conectador al server
-        /* socket.emit('Mensaje de Bienvenido', {
-            msg: 'Bienvenido al Server',
-            fecha: new Date(),
-        }); */
+            // Escuchar evento mensaje del cliente
+            socket.on('mensaje-to-server', (data) => {
+                console.log(data);
 
-        //Escuchar el evento
-        //mensaje-cliente
-        //console.log (data);
-        socket.on('mensaje-to-server', (data) =>{
-            console.log(data);
-
-            this.io.emit('mensaje-from-server', data);
+                // Emitir mensaje a todos los clientes
+                this.io.emit('mensaje-from-server', data);
+            });
         });
-
-        
-    });
-
     }
-    
-
-
-
 }
-
 
 module.exports = Sockets;
